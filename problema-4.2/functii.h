9307a404431db91b1c0fdb302de026f464d93e17
Serban Amariei-Hondrea <amarieiserban2@gmail.com>
2025-07-29 18:36:03 +0300

    Create functii.h

---
+++ problema-4.2/functii.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTII_H_
+#define FUNCTII_H_
+
+#define DIMMAX 30
+
+void citireVector(int *v, int n);
+void afis(int *v, int n);
+int monede(int *v, int n);
+void ordonareBule(int a[], int n);
+void afis(int a[], int n);
+
+#endif
